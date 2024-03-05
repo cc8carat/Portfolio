c0bfa722425ec9b111019559fa0ecb836f6b5564
cc8carat <zhichao06.ni@gmail.com>
2024-03-05 21:21:29 +0100

    Add about content

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,10 +2,22 @@ function About() {
   return (
     <section className='about'>
       <p>
-        Hi, I'm Zhichao, full stack web developer with 6+ years working experience in E-Commerce, SaaS and tourism, also
-        a very lucky one who found her new favorite thing after 15 weeks training at WBS Coding School. Hence, I decided
-        to turn my career around and dive into the tech world while offering help as a voluntary mentor to those who are
-        on their way to their luck at WBS.
+        A full stack web developer, possessing a strong foundation in frontend best practices, combined with background
+        in product management, business development and e-commerce.
+        <p>
+          After 7 years working in business development, as the most tech-savvy person in non-technical teams, I turned
+          my career around and dived into the tech world. As lucky as I always was, I found my new favorite thing,
+          programming. I am passionate about solving problems and puzzles, building connections between people and
+          systems, testing new technologies and recipes. I enjoy taking challenges as well as helping others to achieve
+          theirs.A dedicated Frontend Engineer. An open and receptive mind. A dynamic start-up spirit. With deep
+          proficiency in creating user-centric application using cutting-edge technologies like React, Next.js and
+          TypeScript. Possesses a strong foundation in frontend best practices, combined with background in product
+          management, business development and e-commerce. After 7 years working in business development, as the most
+          tech-savvy person in non-technical teams, I turned my career around and dived into the tech world. As lucky as
+          I always was, I found my new favorite thing, programming. I am passionate about solving problems and puzzles,
+          building connections between people and systems, testing new technologies and recipes. I enjoy taking
+          challenges as well as helping others to achieve theirs.
+        </p>
       </p>
       <p>
         I am passionate about solving problems and puzzles, building connections between people and systems, testing new
